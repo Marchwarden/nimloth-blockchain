01f3c689d3fdafaedcdf6642aa72ba58b905ce83
JohnBartleyDev <bartleyj5064@gmail.com>
2022-11-14 18:08:37 -0500

    added basic conflict resolution
    	-still need to add interaction ui to pass arguments

--- src/BlockchainClass.py
+++ src/BlockchainClass.py
@@ -1,4 +1,5 @@
 import time
+from urllib import request
 import urllib.parse
 
 from dataclasses import dataclass, field
@@ -27,7 +28,35 @@ class Blockchain:
         passed_url = urllib.parse.urlparse(address)
         self.nodes.add(passed_url.netloc)
     #conflic resolution 
+    def resolve_conflicts(self):
 
+        neighbours = self.nodes
+        new_chain = None
+
+        # We're only looking for chains longer than ours
+        max_length = len(self.chain)
+
+        # Grab and verify the chains from all the nodes in our network
+        for node in neighbours:
+            response = request.get(f'http://{node}/chain')
+
+            if response.status_code == 200:
+                length = response.json()['length']
+                chain = response.json()['chain']
+
+                # Check if the length is longer and the chain is valid
+                if length > max_length and self.valid_chain(chain):
+                    max_length = length
+                    new_chain = chain
+
+        # Replace our chain if we discovered a new, valid chain longer than ours
+        if new_chain:
+            self.chain = new_chain
+            return True
+
+        return False
+    
+    
     def proof_of_work(self, block: NimlothBlock) -> str:
         block.nonce = 0
         computed_hash = block.compute_hash()
--- src/app.py
+++ src/app.py
@@ -1,10 +1,19 @@
 from flask import Flask
-
+import BlockchainClass
 app = Flask(__name__)
 
-@app.route('/') 
-def test_message(): 
-   return '<h1>Sup2</h1>'
+# Generate a globally unique address for this node
 
-if __name__ == "__main__": 
-   app.run(debug=True, host='0.0.0.0', port=8000)
+
+# Instantiate the Blockchain
+blockchain = Blockchain()
+
+if __name__ == '__main__':
+    from argparse import ArgumentParser
+
+    parser = ArgumentParser()
+    parser.add_argument('-p', '--port', default=5000, type=int, help='port to listen on')
+    args = parser.parse_args()
+    port = args.port
+
+    app.run(host='0.0.0.0', port=port)
