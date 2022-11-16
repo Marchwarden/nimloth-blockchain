522dce9dbdba17e71c3cc104e183d50021fee20c
Alex Liu <junhual07@gmail.com>
2022-11-16 01:59:59 -0500

    reconfigure app factory

---
+++ core/__init__.py
@@ -0,0 +1,22 @@
+from flask import Flask
+
+
+def create_app(test_config=None):
+    app = Flask(__name__, instance_relative_config=True)
+    app.config.from_mapping(
+        SECRET_KEY="dev",
+    )
+
+    if test_config is None:
+        # load the instance config, if it exists, when not testing
+        app.config.from_pyfile("config.py", silent=True)
+    else:
+        # load the test config if passed in
+        app.config.from_mapping(test_config)
+
+    # a simple page that says hello
+    @app.route("/")
+    def hello():
+        return "Hello, World!"
+
+    return app
